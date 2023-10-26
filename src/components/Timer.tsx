42368fd2926d6cc7522b9346c46244913950c486
wagao <wagao1029@gmail.com>
2023-10-26 18:25:45 +0900

    Add Timer to PhotoDialog

---
+++ src/components/Timer.tsx
@@ -0,0 +1,39 @@
+import { memo, useEffect, useState } from 'react';
+
+type Props = {
+  initTime: number;
+  className: string;
+};
+
+const Timer = memo(function TimerBase({ initTime, className }: Props) {
+  const [countTime, setCountTime] = useState<number>(initTime);
+
+  useEffect(() => {
+    const countDownInterval = setInterval(() => {
+      if (countTime > 0) {
+        setCountTime((t) => t - 1);
+      } else {
+        clearInterval(countDownInterval);
+      }
+    }, 1000);
+    return () => {
+      clearInterval(countDownInterval);
+    };
+  }, []);
+
+  const hr = Math.floor(countTime / (60 * 60))
+    .toString()
+    .padStart(2, '0');
+  const min = Math.floor((countTime / 60) % 60)
+    .toString()
+    .padStart(2, '0');
+  const sec = (countTime % 60).toString().padStart(2, '0');
+
+  return (
+    <p className={className}>
+      expires: {hr}:{min}:{sec}
+    </p>
+  );
+});
+
+export default Timer;
