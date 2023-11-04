5ddccb47a99e9310b7e66a2a930899e2f33e6a49
wagao <wagao1029@gmail.com>
2023-11-05 02:48:32 +0900

    画像読み込み時にローディングアイコンを表示

--- src/components/dialogs/PhotoDialog.tsx
+++ src/components/dialogs/PhotoDialog.tsx
@@ -23,11 +23,11 @@ export const PhotoDialog = memo(function PhotoDialogBase({ mapPhotos, mapRef, on
   const [currentIdx, setCurrentIdx] = useState<number>(0);
   const [remainingTime, setRemainingTime] = useState<number>(getRemainingTime(mapPhotos[0].date));
   const [isExpired, setIsExpired] = useState<boolean>(false);
-  const [isImgLoading, setIsImgLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const onClickPrevBtn = useCallback(() => {
     if (currentIdx > 0) {
-      setIsImgLoading(true);
+      setIsLoading(true);
       setIsExpired(false);
       setRemainingTime(getRemainingTime(mapPhotos[currentIdx - 1].date));
       setCurrentIdx(currentIdx - 1);
@@ -36,7 +36,7 @@ export const PhotoDialog = memo(function PhotoDialogBase({ mapPhotos, mapRef, on
 
   const onClickNextBtn = useCallback(() => {
     if (currentIdx < mapPhotos.length - 1) {
-      setIsImgLoading(true);
+      setIsLoading(true);
       setIsExpired(false);
       setRemainingTime(getRemainingTime(mapPhotos[currentIdx + 1].date));
       setCurrentIdx(currentIdx + 1);
@@ -60,51 +60,46 @@ export const PhotoDialog = memo(function PhotoDialogBase({ mapPhotos, mapRef, on
   return (
     <Dialog height='80%'>
       <CloseButton color={CLOSE_BUTTON_WHITE} onClick={onClose} />
-      <PrevButton onClick={onClickPrevBtn} visible={!isImgLoading && currentIdx > 0} />
+      <PrevButton onClick={onClickPrevBtn} visible={!isLoading && currentIdx > 0} />
       <NextButton
         onClick={onClickNextBtn}
-        visible={!isImgLoading && currentIdx < mapPhotos.length - 1}
+        visible={!isLoading && currentIdx < mapPhotos.length - 1}
       />
-      <div className='h-full overflow-scroll hidden-scrollbar rounded-lg bg-black'>
-        <div className='relative h-full w-full'>
-          <Timer
-            className='absolute z-20 top-2 left-2 text-white'
-            initTime={remainingTime}
-            onExpire={onExpire}
+      <div className='h-full relative overflow-scroll hidden-scrollbar rounded-lg bg-black'>
+        {isLoading && (
+          <Spinner className='absolute z-10 inset-0 flex items-center justify-center bg-black' />
+        )}
+        <Timer
+          className='absolute z-20 top-2 left-2 text-white'
+          initTime={remainingTime}
+          onExpire={onExpire}
+        />
+        {isExpired ? (
+          <img
+            src={iconPhoto}
+            className='absolute inset-0 m-auto max-h-full my-10'
+            onLoad={() => {
+              setIsLoading(false);
+            }}
           />
-          {isImgLoading && (
-            <Spinner className='absolute z-10 inset-0 flex items-center justify-center bg-black' />
-          )}
-          {isExpired ? (
-            <img
-              src={iconPhoto}
-              className='absolute inset-0 m-auto max-h-full my-10'
-              onLoad={() => {
-                setIsImgLoading(false);
-              }}
-            />
-          ) : (
-            <img
-              src={getPhotoUrl(mapPhotos[currentIdx].id)}
-              onLoad={() => {
-                setIsImgLoading(false);
-              }}
-              onError={(e: React.SyntheticEvent<HTMLImageElement>) => {
-                setIsImgLoading(false);
-                e.currentTarget.onerror = null;
-                e.currentTarget.src = iconPhoto;
-              }}
-              className='absolute inset-0 m-auto max-h-full py-10'
-            />
-          )}
-          <p
-            className='absolute z-20 bottom-5 right-1 text-white underline'
-            onClick={onClickAddress}
-          >
-            {mapPhotos[currentIdx].addr}
-          </p>
-          <OsmCopyRight className='absolute z-20 bottom-1 right-1' />
-        </div>
+        ) : (
+          <img
+            src={getPhotoUrl(mapPhotos[currentIdx].id)}
+            onLoad={() => {
+              setIsLoading(false);
+            }}
+            onError={(e: React.SyntheticEvent<HTMLImageElement>) => {
+              setIsLoading(false);
+              e.currentTarget.onerror = null;
+              e.currentTarget.src = iconPhoto;
+            }}
+            className='absolute inset-0 m-auto max-h-full py-10'
+          />
+        )}
+        <p className='absolute z-20 bottom-5 right-1 text-white underline' onClick={onClickAddress}>
+          {mapPhotos[currentIdx].addr}
+        </p>
+        <OsmCopyRight className='absolute z-20 bottom-1 right-1' />
       </div>
     </Dialog>
   );
