dd21c36f07606f779f553ede70d90e7a8d1daf93
wagao <wagao1029@gmail.com>
2023-11-18 16:20:17 +0900

    Add Terms content

--- src/components/dialogs/TermsDialog.tsx
+++ src/components/dialogs/TermsDialog.tsx
@@ -15,196 +15,46 @@ export const TermsDialog = memo(function TermsDialogBase({ onClose }: Props) {
         <h1 className='text-lg pt-6 font-bold'>Terms of Service</h1>
         <div className='overflow-scroll hidden-scrollbar p-4'>
           <ul className='space-y-4 list-inside'>
-            <p className='text-xs'>
-              この利用規約（以下、「本規約」といいます。）は、本ウェブサイト上で提供するサービス（以下、「本サービス」といいます。）の利用条件を定めるものです。
+            <p className='text-sm'>
+              By accessing this website, you agree to be bound by these website terms and all
+              applicable laws and regulations. If you do not agree to any of these terms and
+              conditions, you are prohibited from using or accessing this site.
             </p>
             <li>
-              <span className='text-sm'>第1条（規約の適用）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  本規約は、ユーザーと本サービスの利用に関わる一切の関係に適用されるものとします。
-                </li>
-                <li>
-                  利用者は、本サービスの利用をもって、本規約の内容を承諾頂いたものとみなし、適用されるものとします。
-                </li>
-              </ol>
+              <span className='font-bold'>1. Disclaimer</span>
+              <div className='text-sm'>
+                The operator of this service is not responsible for any damage caused to users due
+                to this service. We are also not responsible for the content of images uploaded by
+                users. In addition, users are responsible for managing the copyright and other
+                rights of uploaded images.
+              </div>
             </li>
             <li>
-              <span className='text-sm'>第2条（利用登録）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  押したスタンプのデータ保持やスタンプの作成には、Googleアカウントでのログインが必要であり、
-                  利用者は、アカウントを管理する責任を負うものとします。
-                </li>
-                <li>
-                  ユーザーID及びパスワードが第三者によって使用されたことによって生じた損害は、
-                  当社に故意又は重大な過失がある場合を除き、当社は一切の責任を負わないものとします。
-                </li>
-              </ol>
+              <span className='font-bold'>2. Changes to the Terms of Service</span>
+              <div className='text-sm'>
+                The operator of this service may change the terms at any time without notifying
+                users if it deems it necessary. If the User starts using the Service after these
+                terms have been changed, the User will be deemed to have agreed to the changed
+                terms.
+              </div>
             </li>
             <li>
-              <span className='text-sm'>第3条（禁止事項）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                ユーザーは、本サービスの利用にあたり、以下の行為をしてはなりません。
-                <li>法令または公序良俗に違反する行為</li>
-                <li>犯罪行為に関連する行為</li>
-                <li>
-                  当社、本サービスの他のユーザー、または第三者のサーバーまたはネットワークの機能を破壊したり、妨害したりする行為
-                </li>
-                <li>当社のサービスの運営を妨害するおそれのある行為</li>
-                <li>他のユーザーに関する個人情報等を収集または蓄積する行為</li>
-                <li>不正アクセスをし、またはこれを試みる行為</li>
-                <li>他のユーザーに成りすます行為</li>
-                <li>
-                  当社のサービスに関連して、反社会的勢力に対して直接または間接に利益を供与する行為
-                </li>
-                <li>
-                  当社、本サービスの他のユーザーまたは第三者の知的財産権、肖像権、プライバシー、名誉その他の権利または利益を侵害する行為
-                </li>
-                <li>
-                  以下の表現を含み、または含むと当社が判断する内容を本サービス上に投稿し、または送信する行為
-                  <ol className='ml-1 list-disc list-inside'>
-                    <li>過度に暴力的な表現</li>
-                    <li>露骨な性的表現</li>
-                    <li>人種、国籍、信条、性別、社会的身分、門地等による差別につながる表現</li>
-                    <li>その他反社会的な内容を含み他人に不快感を与える表現</li>
-                  </ol>
-                </li>
-                <li>
-                  以下を目的とし、または目的とすると当社が判断する行為
-                  <ol className='ml-1 list-disc list-inside'>
-                    <li>
-                      営業、宣伝、広告、勧誘、その他営利を目的とする行為（当社の認めたものを除きます。）
-                    </li>
-                    <li>他のユーザーに対する嫌がらせや誹謗中傷を目的とする行為</li>
-                    <li>
-                      当社、本サービスの他のユーザー、または第三者に不利益、損害または不快感を与えることを目的とする行為
-                    </li>
-                    <li>
-                      その他本サービスが予定している利用目的と異なる目的で本サービスを利用する行為
-                    </li>
-                  </ol>
-                </li>
-                <li>その他、当社が不適切と判断する行為</li>
-              </ol>
+              <span className='font-bold'>
+                3. Change or suspension of the provision of the Service
+              </span>
+              <div className='text-sm'>
+                The content of this service may change without prior notice to users. In addition,
+                we may suspend or cancel all or part of the services we provide without prior
+                notice.
+              </div>
             </li>
             <li>
-              <span className='text-sm'>第4条（本サービスの提供の停止等）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  当社は、以下のいずれかの事由があると判断した場合、
-                  ユーザーに事前に通知することなく本サービスの全部または一部の提供を停止または中断することができるものとします。
-                  <ol className='ml-1 list-disc list-inside'>
-                    <li>本サービスにかかるコンピュータシステムの保守点検または更新を行う場合</li>
-                    <li>天災などの不可抗力により、本サービスの提供が困難となった場合</li>
-                    <li>コンピュータまたは通信回線等が事故により停止した場合</li>
-                    <li>その他、当社が本サービスの提供が困難と判断した場合</li>
-                  </ol>
-                </li>
-                <li>
-                  当社は、本サービスの提供の停止または中断により、ユーザーまたは第三者が被ったいかなる不利益または損害についても、
-                  一切の責任を負わないものとします。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第5条（著作権）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  ユーザーは、自ら著作権等の必要な知的財産権を有するか、または必要な権利者の許諾を得た文章や画像に関してのみ、
-                  スタンプ作成に使用する画像やユーザープロフィール画像としてアップロードをすることができるものとします。
-                </li>
-                <li>
-                  ユーザーが本サービスを利用してアップロードした文章、画像等の著作権については、当該ユーザーその他既存の権利者に留保されるものとします。
-                  ただし、当社は、本サービスを利用してアップロードされた文章、画像等について、
-                  本サービスの改良、品質の向上、または不備の是正等ならびに本サービスの周知宣伝等に必要な範囲で利用できるものとし、
-                  ユーザーは、この利用に関して、著作者人格権を行使しないものとします。
-                </li>
-                <li>
-                  前項本文の定めるものを除き、本サービスおよび本サービスに関連する一切の情報についての著作権および
-                  その他の知的財産権はすべて当社または当社にその利用を許諾した権利者に帰属し、
-                  ユーザーは無断で複製、譲渡、貸与、翻訳、改変、転載、公衆送信、伝送、配布、出版、営業使用等をしてはならないものとします。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第6条（利用制限および登録抹消）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  当社は、ユーザーが以下のいずれかに該当する場合には、事前の通知なく、投稿データを削除し、
-                  ユーザーに対して本サービスの全部もしくは一部の利用を制限しまたはユーザーとしての登録を抹消することができるものとします。
-                  <ol className='ml-1 list-disc list-inside'>
-                    <li>本規約のいずれかの条項に違反した場合</li>
-                    <li>登録事項に虚偽の事実があることが判明した場合</li>
-                    <li>当社からの連絡に対し、一定期間返答がない場合</li>
-                    <li>その他、当社が本サービスの利用を適当でないと判断した場合</li>
-                  </ol>
-                </li>
-                <li>
-                  当社は、本条に基づき当社が行った行為によりユーザーに生じた損害について、一切の責任を負いません。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第7条（退会）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  ユーザーは、当社の定める退会手続により、本サービスから退会できるものとします。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第8条（保証の否認および免責事項）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  当社は、本サービスに事実上または法律上の瑕疵
-                  （安全性、信頼性、正確性、完全性、有効性、特定の目的への適合性、セキュリティなどに関する欠陥、エラーやバグ、権利侵害などを含みます。）
-                  がないことを明示的にも黙示的にも保証しておりません。
-                </li>
-                <li>
-                  当社は、本サービスに起因してユーザーに生じたあらゆる損害について一切の責任を負いません。
-                  ただし、本サービスに関する当社とユーザーとの間の契約（本規約を含みます。）
-                  が消費者契約法に定める消費者契約となる場合、この免責規定は適用されません。
-                </li>
-                <li>
-                  当社は、本サービスに関して、ユーザーと他のユーザーまたは第三者との間において生じた取引、連絡または紛争等について一切責任を負いません。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第9条（サービス内容の変更等）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  当社は、ユーザーに通知することなく、本サービスの内容を変更しまたは本サービスの提供を中止することができるものとし、
-                  これによってユーザーに生じた損害について一切の責任を負いません。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第10条（利用規約の変更）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  当社は、必要と判断した場合には、ユーザーに通知することなくいつでも本規約を変更することができるものとします。
-                  なお、本規約の変更後、本サービスの利用を開始した場合には、当該ユーザーは変更後の規約に同意したものとみなします。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第11条（個人情報の取扱い）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>
-                  当社は、本サービスの利用によって取得する個人情報については、当社「プライバシーポリシー」に従い適切に取り扱うものとします。
-                </li>
-              </ol>
-            </li>
-            <li>
-              <span className='text-sm'>第12条（準拠法・裁判管轄）</span>
-              <ol className='space-y-1 text-xs list-decimal list-inside'>
-                <li>本規約の解釈にあたっては、日本法を準拠法とします。</li>
-                <li>
-                  本サービスに関して紛争が生じた場合には、当社の本店所在地を管轄する裁判所を専属的合意管轄とします。
-                </li>
-              </ol>
+              <span className='font-bold'>4. Governing Law and Jurisdiction</span>
+              <div className='text-sm'>
+                These terms shall be governed by and construed in accordance with Japanese law. In
+                the event of a dispute regarding this service, the court with jurisdiction over the
+                location of the operator shall have exclusive jurisdiction.
+              </div>
             </li>
           </ul>
         </div>
